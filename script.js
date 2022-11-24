fcd765d2958ef3a0f86b7ea6721c916454079c5c
Lellisinthewoods <ellinor.romin@hotmail.com>
2022-11-24 13:50:36 +0100

    added more functions. they don't work.

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 const BASE_URL = `https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false`;
-let main = document.querySelector(`main`)
+let main = document.querySelector(`main`);
+let goalCode;
+let goal;
 
-async function listGoals(BASE_URL){
+
+async function listGoals(BASE_URL)
+{
     //debugger;
     const resp = await fetch(BASE_URL)
     const data = await resp.json();
+    console.log(data);
     printGoals(data)
 }
 
-function printGoals(data){
+function printGoals(data)
+{
     //skicka in målet i den här funktionen
     //lägg in alla mål i HTML-koden med adjacentHTML
     for (let i = 0; i < data.length; i++) {
-        const goal = data[i].title;
+        goal = data[i].title;
+        goalCode = data[i].code;
         let template = `<button>${goal}</button>`;
         main.insertAdjacentHTML(`beforeend`, template)
-        
-        let buttons = document.querySelectorAll(`button`);
-        buttons.forEach(btn => {
-            btn.addEventListener(`click`, () =>{
-                //skriv ut information om målet man klickade på
-            })
-        });
+
     }
+
+    let buttons = document.querySelectorAll(`button`);
+    buttons.forEach(btn => {
+        btn.addEventListener(`click`, () =>{
+            if (btn.innerHTML === goal){} //den skriver bara ut 17
+            goalInfo(goalCode)
+        })
+    });
+}
+
+async function goalInfo(goalCode)
+{
+    new_Url = `https://unstats.un.org/SDGAPI/v1/sdg/Goal/${goalCode}/List?includechildren=false`
+    const resp = await fetch(new_Url)
+    const newData = await resp.json();
+    console.log(newData);
 }
 
 listGoals(BASE_URL);
